ff25350c6880c55acd8f0e2464d9c647ccd8d60f
Andrew Culkin <andrewsculkin@gmail.com>
2021-04-26 12:51:08 -0400

    Fix deploy error with Typescript

--- components/HomePageCard/index.jsx
+++ components/HomePageCard/index.jsx
@@ -1,16 +1,7 @@
 import Link from 'next/link'
 import { Card, Icon } from 'semantic-ui-react'
 
-interface PageInfo {
-  page: {
-    link: string
-    name: string
-    iconName: string
-    description: string
-  }
-}
-
-export const HomePageCard: React.FC<PageInfo> = ({ page }) => {
+export const HomePageCard = ({ page }) => {
   const { link, name, iconName, description } = page
   return (
     <Link href={link}>
