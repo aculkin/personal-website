4136aa428dd211bb942e7a3f5536cdb41b2b40f2
Andrew Culkin <andrewsculkin@gmail.com>
2021-04-29 10:20:20 -0400

    Update dependencies

--- utility/languages.ts
+++ utility/languages.ts
@@ -4,7 +4,7 @@ export const languages = [
     shortDescription: 'Popular language for interactive web development',
     proficiency: 3,
     description:
-      'I’ve always been a proponent of Javascript as a front end and back end language, especially when it comes to web based projects. In addition, the work required to get a new project up and running is minimal compared to other languages so it makes sense in the fast paced world of startups. A strictly typed language like Java would be good for companies willing to spend more time on development, but in a startup, speed is arguably most important. In my experience managing younger teams, it is much easier to have a single language that we can use on both the front end and the back end.',
+      'I’ve always been a proponent of Javascript as a front end and back end language, especially when it comes to web based projects. The work required to get a new project up and running is minimal compared to other languages so it makes sense in the fast paced world of startups. A strictly typed language would be good for larger companies willing to spend more time on development, but in a startup speed, proof of concept, and time to market are all more important than building a perfect first product. In my experience managing younger teams, it is much easier to have a single language that we can use on both the front end and the back end.',
     link: 'https://www.javascript.com/',
     imgLink: '/tech/javascript.png',
   },
@@ -13,7 +13,7 @@ export const languages = [
     shortDescription: 'Typed version of Javascript developed by Microsoft',
     proficiency: 2,
     description:
-      'Typescript has all the good parts about Javascript and also fixes some of the issues many have with the language. Typing does take a little more development time, but the end result is worth it due to several factors. Diagnosing errors before they happen becomes much easier, and the likelihood of mistakes from accessing unexpected variable types decrease as well. I’ve often taken the approach of prototyping with javascript, just to get something up and running, and then transitioning to typescript slowly once the proof of concept exists and the product requirements are more defined.',
+      'Typescript has all the good parts about Javascript and also fixes some of the issues many have with the language. Typing does take a little more development time, but the end result is worth it due to several factors. Diagnosing errors before they happen becomes much easier, and the likelihood of mistakes from accessing unexpected variable types decrease as well. I’ve often taken the approach of prototyping with javascript, just to get something up and running, and then transitioning to typescript slowly once the proof of concept exists and the product requirements are more defined. This website is actually written with like 95% typescript the last time I checked on Github.',
     link: 'https://www.typescriptlang.org/',
     imgLink: '/tech/typescript.png',
   },
