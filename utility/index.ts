75494fd5e87d0688271c80091903cf55c11b881e
Andrew Culkin <andrewsculkin@gmail.com>
2021-05-01 15:50:01 -0400

    Remove Json objects that are now retrieved via API

--- utility/index.ts
+++ utility/index.ts
@@ -1,8 +1,3 @@
 export { default as toast } from './toast'
-export { default as languages } from './languages'
-export { default as frameworks } from './frameworks'
-export { default as technologies } from './technologies'
-export { default as education } from './education'
-export { default as employment } from './employment'
 export { default as projects } from './projects'
 export { default as pages } from './pages'
