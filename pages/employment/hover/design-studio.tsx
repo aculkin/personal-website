8affe3dba91006de49634cb31b9a77127bf5688e
Andrew Culkin <andrew.culkin@hover.to>
2022-08-18 09:22:23 -0400

    feat: add design studio page

---
+++ pages/employment/hover/design-studio.tsx
@@ -0,0 +1,9 @@
+export const DesignStudioPage: React.FC= () => {
+    return (
+        <div style={{height: '100vh', width: '100vw'}}>
+            <iframe frameBorder="0" src='https://hover.to/design-studio/guest' height='100%' width="100%" />
+        </div>
+    )
+}
+
+export default DesignStudioPage
