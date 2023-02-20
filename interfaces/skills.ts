7472c8ab1d7c5958bedfa485de812c23ddd67d03
aculkin <andrew.culkin@hover.to>
2023-02-20 11:00:24 -0500

    feat: make skill type an enum

--- interfaces/skills.ts
+++ interfaces/skills.ts
@@ -1,20 +1,22 @@
 export interface SkillInterface {
-  id: number
-  name: string
-  shortDescription: string
-  proficiency: number
-  description: string
-  link: string
-  awsKey: string
+	id: number;
+	name: string;
+	shortDescription: string;
+	proficiency: number;
+	description: string;
+	link: string;
+	awsKey: string;
 }
 
+type SkillType = "language" | "technology" | "framework";
+
 export interface SkillInfoInterface {
-  type: string
-  skill: SkillInterface
+	type: SkillType;
+	skill: SkillInterface;
 }
 
 export interface SkillsInterface {
-  languages: [SkillInterface]
-  technologies: [SkillInterface]
-  frameworks: [SkillInterface]
+	languages: [SkillInterface];
+	technologies: [SkillInterface];
+	frameworks: [SkillInterface];
 }
