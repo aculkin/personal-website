d5a680664a3e6d192421c68280322d3cb9af15d9
aculkin <andrew.culkin@hover.to>
2023-02-20 11:00:37 -0500

    feat: re-order lanaguages to be first

--- pages/skills/index.tsx
+++ pages/skills/index.tsx
@@ -1,126 +1,127 @@
-import * as React from 'react'
-import Head from 'next/head'
-import { GetStaticProps } from 'next'
-import { Card, Divider, Header, Container } from 'semantic-ui-react'
+import * as React from "react";
+import Head from "next/head";
+import { GetStaticProps } from "next";
+import { Card, Divider, Header, Container } from "semantic-ui-react";
 
-import SkillItem from '../../components/SkillItem'
-import { SkillsInterface } from '../../interfaces/skills'
-import { API } from '../../API'
-import { MainLayout } from '../../layouts/main-layout'
+import SkillItem from "../../components/SkillItem";
+import { SkillsInterface } from "../../interfaces/skills";
+import { API } from "../../API";
+import { MainLayout } from "../../layouts/main-layout";
 
 const pageStyle = {
-  backgroundImage: 'url(/skills-background.jpg)',
-  backgroundSize: 'cover',
-  backgroundAttachment: 'fixed',
-  paddingBottom: '50px',
-}
+	backgroundImage: "url(/skills-background.jpg)",
+	backgroundSize: "cover",
+	backgroundAttachment: "fixed",
+	paddingBottom: "50px",
+};
 
 const headerStyle = {
-  paddingTop: '2em',
-  fontSize: '3em',
-  fontWeight: 'bold',
-  color: 'white',
-  textShadow: '2px 2px 8px #000000',
-}
+	paddingTop: "2em",
+	fontSize: "3em",
+	fontWeight: "bold",
+	color: "white",
+	textShadow: "2px 2px 8px #000000",
+};
 
 const subheaderStyle1 = {
-  fontSize: '.4em',
-  color: 'white',
-  textShadow: '2px 2px 8px #000000',
-}
+	fontSize: ".4em",
+	color: "white",
+	textShadow: "2px 2px 8px #000000",
+};
 
 const subheaderStyle2 = {
-  fontSize: '1.75em',
-  fontWeight: 'bold',
-  color: 'white',
-  textShadow: '2px 2px 8px #000000',
-}
+	fontSize: "1.75em",
+	fontWeight: "bold",
+	color: "white",
+	textShadow: "2px 2px 8px #000000",
+};
 
 export const SkillsPage: React.FC<SkillsInterface> = ({
-  languages,
-  technologies,
-  frameworks,
+	languages,
+	technologies,
+	frameworks,
 }) => {
-  return (
-    <MainLayout>
-      <Head>
-        <title>Andrew Culkin | Skills</title>
-        <meta
-          name="description"
-          content="All the Languages, Technologies and Development Frameworks I've worked with over my career."
-        />
-      </Head>
-      <div style={pageStyle}>
-        <Container>
-          <Header style={headerStyle} textAlign="center" as="h1">
-            Skills
-            <Header.Subheader style={subheaderStyle1}>
-              Frameworks, Technologies, and Languages I&apos;ve worked with
-            </Header.Subheader>
-          </Header>
-          <Divider hidden />
-        </Container>
-        <Container>
-          <Divider />
-          <Header style={subheaderStyle2} textAlign="center" as="h2">
-            Frameworks
-          </Header>
-          <Divider hidden />
-          <Card.Group stackable doubling centered itemsPerRow={5}>
-            {frameworks?.map((skill, index) => (
-              <SkillItem
-                skill={skill}
-                key={skill?.name || index}
-                type="framework"
-              />
-            ))}
-          </Card.Group>
-          <Divider />
-          <Header style={subheaderStyle2} textAlign="center" as="h2">
-            Technologies
-          </Header>
-          <Divider hidden />
-          <Card.Group stackable doubling centered itemsPerRow={5}>
-            {technologies?.map((skill, index) => (
-              <SkillItem
-                skill={skill}
-                key={skill?.name || index}
-                type="technology"
-              />
-            ))}
-          </Card.Group>
-          <Divider hidden />
-          <Divider />
-          <Header style={subheaderStyle2} textAlign="center" as="h2">
-            Languages
-          </Header>
-          <Divider hidden />
-          <Card.Group stackable doubling centered itemsPerRow={5}>
-            {languages?.map((skill, index) => (
-              <SkillItem
-                skill={skill}
-                key={skill?.name || index}
-                type="language"
-              />
-            ))}
-          </Card.Group>
-        </Container>
-      </div>
-    </MainLayout>
-  )
-}
+	return (
+		<MainLayout>
+			<Head>
+				<title>Andrew Culkin | Skills</title>
+				<meta
+					name='description'
+					content="All the Languages, Technologies and Development Frameworks I've worked with over my career."
+				/>
+			</Head>
+			<div style={pageStyle}>
+				<Container>
+					<Header style={headerStyle} textAlign='center' as='h1'>
+						Skills
+						<Header.Subheader style={subheaderStyle1}>
+							Frameworks, Technologies, and Languages I&apos;ve worked with
+						</Header.Subheader>
+					</Header>
+					<Divider hidden />
+				</Container>
+				<Container>
+					<Divider />
+					<Header style={subheaderStyle2} textAlign='center' as='h2'>
+						Languages
+					</Header>
+					<Divider hidden />
+					<Card.Group stackable doubling centered itemsPerRow={5}>
+						{languages?.map((skill, index) => (
+							<SkillItem
+								skill={skill}
+								key={skill?.name || index}
+								type='language'
+							/>
+						))}
+					</Card.Group>
+					<Divider />
+					<Header style={subheaderStyle2} textAlign='center' as='h2'>
+						Frameworks
+					</Header>
+					<Divider hidden />
+					<Card.Group stackable doubling centered itemsPerRow={5}>
+						{frameworks?.map((skill, index) => (
+							<SkillItem
+								skill={skill}
+								key={skill?.name || index}
+								type='framework'
+							/>
+						))}
+					</Card.Group>
+					<Divider />
+					<Header style={subheaderStyle2} textAlign='center' as='h2'>
+						Technologies
+					</Header>
+					<Divider hidden />
+					<Card.Group stackable doubling centered itemsPerRow={5}>
+						{technologies?.map((skill, index) => (
+							<SkillItem
+								skill={skill}
+								key={skill?.name || index}
+								type='technology'
+							/>
+						))}
+					</Card.Group>
+					<Divider hidden />
+					<Divider />
+				</Container>
+			</div>
+		</MainLayout>
+	);
+};
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { languages } = await API.languages.loadAll()
-  const { technologies } = await API.technologies.loadAll()
-  const { frameworks } = await API.frameworks.loadAll()
-  return {
-    props: {
-      languages,
-      technologies,
-      frameworks,
-    },
-  }
-}
+	const { languages } = await API.languages.loadAll();
+	const { technologies } = await API.technologies.loadAll();
+	const { frameworks } = await API.frameworks.loadAll();
+	return {
+		props: {
+			languages,
+			technologies,
+			frameworks,
+		},
+	};
+};
 
-export default SkillsPage
+export default SkillsPage;
